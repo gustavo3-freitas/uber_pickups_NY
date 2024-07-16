06e8a01ee81b7ec800e07607bbc1d0f85125076b
Gustavo <149829646+gustavo3-freitas@users.noreply.github.com>
2024-07-16 20:30:34 -0300

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -35,34 +35,6 @@ st.markdown(
 
 
 
-def intro():
-
-
-    st.write("# Welcome to Streamlit! 👋")
-    st.sidebar.success("Select a demo above.")
-
-    st.markdown(
-        """
-        Streamlit is an open-source app framework built specifically for
-        Machine Learning and Data Science projects.
-
-        **👈 Select a demo from the dropdown on the left** to see some examples
-        of what Streamlit can do!
-
-        ### Want to learn more?
-
-        - Check out [streamlit.io](https://streamlit.io)
-        - Jump into our [documentation](https://docs.streamlit.io)
-        - Ask a question in our [community
-          forums](https://discuss.streamlit.io)
-
-        ### See more complex demos
-
-        - Use a neural net to [analyze the Udacity Self-driving Car Image
-          Dataset](https://github.com/streamlit/demo-self-driving)
-        - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
-    """
-    )
 
 def uber_pickups():
 
@@ -72,14 +44,16 @@ def uber_pickups():
     )
 
 
-st.title('Uber pickups in NYC')
+st.title('Streamlit Examples')
+
+
+@st.cache_data
+def load_data(nrows):
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
             'streamlit-demo-data/uber-raw-data-sep14.csv.gz')
 
-@st.cache_data
-def load_data(nrows):
     data = pd.read_csv(DATA_URL, nrows=nrows)
     lowercase = lambda x: str(x).lower()
     data.rename(lowercase, axis='columns', inplace=True)
