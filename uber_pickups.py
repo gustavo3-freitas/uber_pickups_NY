9af9b5dcbc1391bbd9d21ad8210cd9e2bd734cb8
Gustavo <149829646+gustavo3-freitas@users.noreply.github.com>
2024-07-16 20:04:36 -0300

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -2,6 +2,12 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 
+def unserializable_data():
+		return lambda x: x
+
+#👇 results in an exception when enforceSerializableSessionState is on
+st.session_state.unserializable = unserializable_data()
+
 # Initialization
 if 'key' not in st.session_state:
     st.session_state['key'] = 'value'
@@ -24,6 +30,33 @@ st.session_state
 
 st.write(st.session_state['value'])
 
+
+# Delete a single key-value pair
+del st.session_state[key]
+
+# Delete all the items in Session state
+for key in st.session_state.keys():
+    del st.session_state[key]
+
+st.text_input("Your name", key="name")
+
+# This exists now:
+st.session_state.name
+
+def form_callback():
+    st.write(st.session_state.my_slider)
+    st.write(st.session_state.my_checkbox)
+
+with st.form(key='my_form'):
+    slider_input = st.slider('My slider', 0, 10, 5, key='my_slider')
+    checkbox_input = st.checkbox('Yes or No', key='my_checkbox')
+    submit_button = st.form_submit_button(label='Submit', on_click=form_callback)
+
+# .streamlit/config.toml
+[runner]
+enforceSerializableSessionState = true
+
+#---------
 
 st.title('Uber pickups in NYC')
 
