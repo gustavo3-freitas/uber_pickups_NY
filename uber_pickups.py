4964bc92938c290d97e76654080ddb1c325de996
Gustavo <149829646+gustavo3-freitas@users.noreply.github.com>
2024-07-16 20:13:37 -0300

    Update uber_pickups.py

--- uber_pickups.py
+++ uber_pickups.py
@@ -1,7 +1,7 @@
 import streamlit as st
 import pandas as pd
 import numpy as np
-from urllib.error import URLError
+#from urllib.error import URLError
 
 st.set_page_config(
     page_title="Hello",
